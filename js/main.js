c7ec2f417fb8858e7813c1a4c5f9c425be9cd08b
Nawin Kumar <nawinkumar@trdfin.in>
2024-08-01 19:20:22 +0530

    Nawin

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+// Cursor
+
+let innerCursor = document.querySelector(".inner-cursor");
+let outerCursor = document.querySelector(".outer-cursor");
+
+document.addEventListener("mousemove", moveCursor);
+
+function moveCursor(e){
+    let x = e.clientX;
+    let y = e.clientY;
+    innerCursor.style.left = `${x}px`;
+    innerCursor.style.top = `${y}px`;
+    outerCursor.style.left = `${x}px`;
+    outerCursor.style.top = `${y}px`;
+}
+let links = Array.from(document.querySelectorAll("a"));
+links.forEach(link => {
+    link.addEventListener("mouseover", () => {
+        innerCursor.classList.add("grow");
+    });
+    link.addEventListener("mouseleave", () => {
+        innerCursor.classList.remove("grow");
+    });
+});
+
 // Navbar
 function toggleContainerClass() {
     if (window.innerWidth < 992) {
@@ -69,4 +94,3 @@ window.addEventListener("load",function(){
 document.querySelector('.exitBtn').addEventListener("click",function(){
     document.querySelector(".otp-Form").style.display = "none";
 });
-
